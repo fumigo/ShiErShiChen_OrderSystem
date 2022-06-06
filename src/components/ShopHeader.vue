aa4eb6a5dde46eefcbcb2523d6bb7a0d9a6895a0
cen1234 <mh3030027737@163.com>
2022-06-06 12:43:07 +0800

    ShopHeader.vue上传

---
+++ src/components/ShopHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="shop-header">
+        <nav class="shop-nav" :style="{backgroundImage: `url(../assets/image/dessert/dessert1.jpg)`}">
+
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShopHeader.vue",
+    }
+</script>
+
+<style lang="stylus"  rel="stylesheet/stylus" scoped>
+    @import "../assets/stylus/mixins.styl"
+
+    .shop-header
+        height 100%
+        position relative
+        background #fff
+        color #fff
+        overflow hidden
+    .shop-nav
+        background-size cover
+        background-repeat no-repeat
+        height 40px
+        padding 5px 10px
+        position relative
+        &::before
+            content ""
+            position absolute
+            left 0
+            right 0
+            top 0
+            bottom 0
+            background-color rgba(119, 103, 137, .43)
+</style>
--- src/views/customer-index.vue
+++ src/views/customer-index.vue
@@ -7,7 +7,7 @@
 <!--        <span class="header_login_text" v-if="!userInfo._id">-->
           登录|注册
 <!--        </span>-->
-            <span class="header_login_text" v-else>
+            <span class="header_login_text" >
            <i class="iconfont icon-person"></i>
         </span>
 <!--        </router-link>-->
